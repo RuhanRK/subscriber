c7fe3d789790fc9fc6e2336e2d994a7b0de81a0b
Ruhan Khandakar <ruhankhandaker@igmail.com>
2019-03-04 07:42:35 +0530

    feat: Done Initial Setup for Add Subscriber Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import Button from "./../Button/Button";
@@ -6,6 +6,32 @@ import Button from "./../Button/Button";
 import "./Form.css";
 
 const Form = () => {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleChange = e => {
+        // set input value into state
+        if (e.target.name === "name") {
+            setName(e.target.value);
+        } else {
+            setPhone(e.target.value);
+        }
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // validate phone number
+        const phoneNumber = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+        if (!phone.match(phoneNumber)) {
+            alert("Enter valid Number");
+        } else {
+            // clear input text
+            setName("");
+            setPhone("");
+        }
+    };
+
     return (
         <div className="form-container">
             <div className="btn-section">
@@ -14,7 +40,7 @@ const Form = () => {
                 </Link>
             </div>
             <div className="form-section">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="name">Name:</label>
                         <input
@@ -22,22 +48,30 @@ const Form = () => {
                             name="name"
                             id="name"
                             placeholder="Your Name"
+                            onChange={handleChange}
+                            autoComplete="off"
+                            value={name}
+                            required
                         />
                     </div>
                     <div className="form-group">
                         <label htmlFor="phone">Phone:</label>
                         <input
                             type="text"
-                            phone="phone"
+                            name="phone"
                             id="phone"
-                            placeholder="Number"
+                            placeholder="Enter Phone Number"
+                            onChange={handleChange}
+                            value={phone}
+                            autoComplete="off"
+                            required
                         />
                     </div>
 
                     <div className="form-group">
                         <h5>Subscriber to Added: </h5>
-                        <p>Name: Shilpa</p>
-                        <p>Phone: 4687897748</p>
+                        <p>Name: {name}</p>
+                        <p>Phone: {phone}</p>
                     </div>
 
                     <Button text="add" />
